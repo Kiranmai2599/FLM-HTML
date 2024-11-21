57d3303d38628a219a4fd8742d817d77598f5e00
Kiranmai2599 <p.kiranmai1999@gmail.com>
2024-11-21 21:21:59 +0530

    day3 done

---
+++ Day3/day3Asgn/2.js
@@ -0,0 +1,6 @@
+let spnElement = document.getElementById("spanElement");
+let rangeInputEle = document.getElementById("rating");
+
+function getrange() {
+    spnElement.textContent = rangeInputEle.value
+}
